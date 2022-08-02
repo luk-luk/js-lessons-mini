85dd50dc2f4581fed608846d0889271b0513de97
luk-luk <iforless@yandex.ru>
2022-08-02 15:31:58 +0400

    js

---
+++ app.js
@@ -0,0 +1,24 @@
+const modal = document.querySelector('#modal')
+const content = document.querySelector('#content')
+const backdrop = document.querySelector('#backdrop')
+
+content.addEventListener('click', openCard)
+backdrop.addEventListener('click', closeModal)
+
+const technologies = [
+    {title: 'HTML', description: 'HTML text', type: 'html', done: true},
+    {title: 'CSS', description: 'CSS text', type: 'css', done: true},
+    {title: 'JavaScript', description: 'JavaScript text', type: 'js', done: false},
+    {title: 'Git', description: 'Git text', type: 'git', done: false},
+    {title: 'REACT', description: 'REACT text', type: 'react', done: false}
+]
+
+function openCard() {
+    modal.classList.add('open')
+}
+
+function closeModal() {
+    modal.classList.remove('open')
+}
+
+console.log(technologies)
